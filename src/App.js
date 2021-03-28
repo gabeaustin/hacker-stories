719e0476e42699b3fc147d80dba59f8eba46d2c9
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-27 20:49:25 -0500

    created an array and mapped thru it

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React from "react";
 
-function getTitle(title) {
-    return title;
-}
+const item = [
+    {
+        title: "React",
+        url: "https://reactjs.org/",
+        author: "Jordan Walke",
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: "Redux",
+        url: "https://redux.js.org/",
+        author: "Dan Abramov, Andrew Clark",
+        num_comments: 2,
+        points: 5,
+        objectID: 1
+    }
+]
+
+const numbers = [1, 4, 9, 16];
+const newNumbers = numbers.map(function(number) {
+    return number * 2;
+});
+
+console.log(newNumbers);
+// 2, 8, 18, 32
+
 
 function App() {
   return (
     <div>
-        <h1>Hello {getTitle("React")}</h1>
 
         <label htmlFor="search">Search: </label>
         <input id="search" type="text" />
+
+        <hr />
+
+        {item.map(function(item) {
+            return <div>{item.title}</div>
+        })}
+
     </div>
   );
 }
